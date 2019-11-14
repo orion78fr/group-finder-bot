2a08861405b768a8015d8fa4dc24afbb904076d9
TURCHINI GUILLAUME <orion78fr@gmail.com>
2019-11-14 15:18:36 +0100

    WIP
     - Add games
     - Format edit

--- src/main.rs
+++ src/main.rs
@@ -45,17 +45,12 @@ fn main() {
     }
     let mut client = client.unwrap();
 
-    let (owners, bot_id) = match client.cache_and_http.http.get_current_application_info() {
-        Ok(info) => {
-            let mut owners = HashSet::new();
-            owners.insert(info.owner.id);
-
-            (owners, info.id)
-        }
+    let (owner, bot_id) = match client.cache_and_http.http.get_current_application_info() {
+        Ok(info) => (info.owner.id, info.id),
         Err(why) => panic!("Could not access application info: {:?}", why)
     };
     println!("Bot id : {}", bot_id);
-    println!("Owners : {:?}", owners);
+    println!("Owner : {}", owner);
 
     {
         let mut data = client.data.write();
@@ -154,26 +149,70 @@ fn add_category(ctx: &mut Context, msg: &Message, mut args: Args) -> CommandResu
         let (chan, msg_to_edit) = guild_games.msg(&guild_id).unwrap();
 
         ctx.http.get_message(chan.0, msg_to_edit.0)
-            .unwrap().edit(&ctx, |m| m.content("Edit"));
+            .unwrap()
+            .edit(&ctx, |m|
+                m.content(format_post(guild_games.categories(&guild_id).unwrap())),
+            );
     }
 
     Ok(())
 }
 
 #[command]
-fn add_game(context: &mut Context, msg: &Message, mut args: Args) -> CommandResult {
+fn add_game(ctx: &mut Context, msg: &Message, mut args: Args) -> CommandResult {
     println!("Command add_game {:?}", args);
+
+    let bot_id = *ctx.data.read().get::<BotId>().unwrap();
+    let guild_id = msg.guild_id.unwrap();
+
+    msg.delete(&ctx); // Don't care if failing
+
+    if args.len() != 3 {
+        let answer = MessageBuilder::new()
+            .push("Wrong number of arguments.\n")
+            .push("Use \"")
+            .mention(&bot_id)
+            .push(" add_game category_name, game_name, emoji\" to add a category")
+            .build();
+
+        match msg.channel_id.say(&ctx.http, &answer) {
+            Ok(m) => {
+                m.react(&ctx,
+                        ReactionType::Unicode(String::from(TRASHCAN))).unwrap();
+            }
+            Err(e) => eprintln!("Error ! Cannot answer !")
+        };
+    } else {
+        let mut data = ctx.data.write();
+        let mut guild_games = data.get_mut::<GuildGames>().unwrap();
+        let category = String::from(args.current().unwrap());
+        let game_name = String::from(args.advance().current().unwrap());
+        let emoji = args.advance().current().unwrap().parse().unwrap();
+
+        guild_games.add_game(&guild_id, &category, game_name, EmojiId(emoji));
+
+        let (chan, msg_to_edit) = guild_games.msg(&guild_id).unwrap();
+
+        ctx.http.get_message(chan.0, msg_to_edit.0)
+            .unwrap()
+            .edit(&ctx, |m|
+                m.content(format_post(guild_games.categories(&guild_id).unwrap())),
+            );
+    }
+
     Ok(())
 }
 
 #[command]
-fn remove_game(context: &mut Context, msg: &Message, mut args: Args) -> CommandResult {
+fn remove_game(ctx: &mut Context, msg: &Message, mut args: Args) -> CommandResult {
+    // TODO delete
     println!("Command remove_game {:?}", args);
     Ok(())
 }
 
 #[command]
-fn remove_category(context: &mut Context, msg: &Message, mut args: Args) -> CommandResult {
+fn remove_category(ctx: &mut Context, msg: &Message, mut args: Args) -> CommandResult {
+    // TODO delete
     println!("Command remove_category {:?}", args);
     Ok(())
 }
@@ -185,9 +224,11 @@ struct Handler;
 impl EventHandler for Handler {
     fn reaction_add(&self, ctx: Context, added_reaction: Reaction) {
         // Only analyze reactions on our own messages, don't care about the rest
-        let reacted_msg = added_reaction.message(&ctx.http).unwrap();
-        if reacted_msg.author.id == *ctx.data.read().get::<BotId>().unwrap() {
-            if added_reaction.emoji == ReactionType::Unicode(String::from(TRASHCAN)) {
+        let bot_id = *ctx.data.read().get::<BotId>().unwrap();
+        if added_reaction.user_id != bot_id {
+            let reacted_msg = added_reaction.message(&ctx.http).unwrap();
+            if reacted_msg.author.id == bot_id {
+                added_reaction.emoji == ReactionType::Unicode(String::from(TRASHCAN));
                 reacted_msg.delete(&ctx);
             }
         }
@@ -198,6 +239,35 @@ impl EventHandler for Handler {
     }
 }
 
+fn format_post(categories: &HashMap<String, Category>) -> String {
+    if categories.is_empty() {
+        return String::from("Please add a category.");
+    }
+
+    let mut mb = MessageBuilder::new();
+
+    categories.values().for_each(|cat| {
+        mb.push_bold_line(cat.name())
+            .push("\n");
+
+        let games = cat.games();
+        if games.is_empty() {
+            mb.push("Please add a game.");
+        } else {
+            games.values().for_each(|game| {
+                // TODO mb.emoji(game.emoji);
+                mb.push(game.emoji.0);
+
+                mb.push(" - ")
+                    .push(&game.name)
+                    .push("\n");
+            });
+        }
+    });
+
+    mb.build()
+}
+
 struct BotId;
 
 impl TypeMapKey for BotId {
@@ -206,7 +276,7 @@ impl TypeMapKey for BotId {
 
 struct GuildGames {
     guild_msg: HashMap<GuildId, (ChannelId, MessageId)>,
-    guild_categories: HashMap<GuildId, HashSet<Category>>,
+    guild_categories: HashMap<GuildId, HashMap<String, Category>>,
 }
 
 impl GuildGames {
@@ -221,7 +291,7 @@ impl GuildGames {
         self.guild_msg.get(guild_id)
     }
 
-    pub fn categories(&self, guild_id: &GuildId) -> Option<&HashSet<Category>> {
+    pub fn categories(&self, guild_id: &GuildId) -> Option<&HashMap<String, Category>> {
         self.guild_categories.get(guild_id)
     }
 
@@ -230,61 +300,47 @@ impl GuildGames {
     }
 
     pub fn add_category(&mut self, guild_id: GuildId, category_name: String) {
-        self.guild_categories.entry(guild_id).or_insert(HashSet::new())
-            .insert(Category::new(category_name));
+        self.guild_categories.entry(guild_id).or_insert(HashMap::new())
+            .insert(category_name.clone(), Category::new(category_name));
+    }
+
+    pub fn add_game(&mut self, guild_id: &GuildId, category_name: &String, name: String, emoji: EmojiId) {
+        self.guild_categories.get_mut(guild_id).unwrap()
+            .get_mut(category_name).unwrap()
+            .add_game(name, emoji);
     }
 }
 
 struct Category {
     name: String,
-    games: HashSet<Game>,
+    games: HashMap<String, Game>,
 }
 
 impl Category {
     pub fn new(name: String) -> Category {
         Category {
             name,
-            games: HashSet::new(),
+            games: HashMap::new(),
         }
     }
 
     pub fn name(&self) -> &String {
         &self.name
     }
-}
 
-impl Hash for Category {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.name.hash(state);
+    pub fn add_game(&mut self, name: String, emoji: EmojiId) {
+        self.games.insert(name.clone(), Game { name, emoji });
+    }
+
+    pub fn games(&self) -> &HashMap<String, Game> {
+        &self.games
     }
 }
-
-impl PartialEq for Category {
-    fn eq(&self, other: &Self) -> bool {
-        self.name.eq(&other.name)
-    }
-}
-
-impl Eq for Category {}
 
 struct Game {
     name: String,
-    emoji: EmojiIdentifier,
+    emoji: EmojiId,
 }
-
-impl Hash for Game {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.name.hash(state);
-    }
-}
-
-impl PartialEq for Game {
-    fn eq(&self, other: &Self) -> bool {
-        self.name.eq(&other.name)
-    }
-}
-
-impl Eq for Game {}
 
 impl TypeMapKey for GuildGames {
     type Value = GuildGames;
